6c462dbb2fe100885872c9acec69be18f4230315
hecker2025 <blitzzcoder7405@gmail.com>
2025-04-12 23:47:33 +0530

    Add files via upload

---
+++ careers/views.py
@@ -0,0 +1,9 @@
+from django.template import loader
+from django.http import HttpResponse
+
+
+def careers(request):
+    #return HttpResponse("welcome!")
+    template = loader.get_template('careershome.html')
+    return HttpResponse(template.render())
+
